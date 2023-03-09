fb6d124b762dbd48f7cdecd035d73a05b5ab79c0
Ilya Sotnikov <ilyasotnikov45@gmail.com>
2023-03-09 19:29:42 +0500

    Update dotfiles

--- copy.sh
+++ copy.sh
@@ -2,6 +2,6 @@
 
 mkdir -p .config/nvim
 mkdir -p .config/Code/User
-cp ~/.config/nvim/init.vim .config/nvim/init.vim
+cp -r ~/.config/nvim .config
 cp ~/.config/Code/User/settings.json .config/Code/User
 code --list-extensions > .config/Code/User/extensions.txt
