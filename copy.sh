87e665e686162753e6a588e8e642fa54ed18901a
Ilya Sotnikov <ilyasotnikov45@gmail.com>
2023-03-11 17:36:12 +0500

    Update dotfiles

--- copy.sh
+++ copy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p .config/nvim
-mkdir -p .config/Code/User
+# mkdir -p .config/Code/User
 cp -r ~/.config/nvim .config
-cp ~/.config/Code/User/settings.json .config/Code/User
-code --list-extensions > .config/Code/User/extensions.txt
+# cp ~/.config/Code/User/settings.json .config/Code/User
+# code --list-extensions > .config/Code/User/extensions.txt
