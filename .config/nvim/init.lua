82970e04dff63cd088c7f7dd773a011abf379a06
Ilya Sotnikov <ilyasotnikov45@gmail.com>
2024-12-14 19:43:23 +0500

    simplify neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,106 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- plugins
+require("lazy").setup({
+    {
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        dependencies = "nvim-lua/plenary.nvim",
+    },
+    "neovim/nvim-lspconfig",
+    {
+        "echasnovski/mini.completion",
+        version = false,
+    },
+    "RRethy/base16-nvim",
+})
+
 vim.g.mapleader = " "
 
-require("plugins")
-require("lsp-cmp")
-require("fuzz")
+-- fuzzy finder
+local telescope_builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", telescope_builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", telescope_builtin.help_tags, {})
+
+-- lsp stuff
+local lspconfig = require("lspconfig")
+
+lspconfig.clangd.setup {
+    capabilities = capabilities,
+    cmd = {
+        "clangd",
+        '--query-driver="/opt/qnx650/host/linux/x86/usr/lib/gcc/i486-pc-nto-qnx6.5.0/4.4.2/cc1plus"'
+    },
+}
+
+lspconfig.lua_ls.setup {}
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set("n", "<space>wl", function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+        vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        -- vim.keymap.set("n", "<space>f", function()
+        --	   vim.lsp.buf.format { async = true }
+        -- end, opts)
+    end,
+})
+
+-- autoformat on save
+-- vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+
+-- switch source/header
+vim.keymap.set("n", "go", "<cmd>ClangdSwitchSourceHeader<cr>")
+
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+
+local imap_expr = function(lhs, rhs)
+    vim.keymap.set('i', lhs, rhs, { expr = true })
+end
+imap_expr('<Tab>', [[pumvisible() ? "\<C-n>" : "\<Tab>"]])
+imap_expr('<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]])
+
+-- completion
+require("mini.completion").setup()
 
 -- 4 spaces
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
-vim.o.expandtab = false
+vim.o.expandtab = true
 vim.o.smarttab = true
 vim.o.smartindent = true
 
@@ -24,7 +117,7 @@ vim.o.colorcolumn = "80,100"
 
 -- colorscheme
 vim.o.termguicolors = true
-vim.cmd("colorscheme base16-one-light")
+vim.cmd("colorscheme base16-gruvbox-material-dark-soft")
 
 vim.o.keymap = "russian-jcukenwin"
 vim.o.iminsert = 0
@@ -32,9 +125,6 @@ vim.o.imsearch = 0
 
 -- explorer
 vim.keymap.set("n", "<C-t>", "<cmd>:Lexplore<cr>")
-
--- pynvim
--- vim.g.python3_host_prog = "~/.pynvim/bin/python"
 
 -- center cursor after scrolling and search
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
